18fb7b61ecae2e2a38cae596b7b3d10803599a06
SeptianSamdany <227007072@student.unsil.ac.id>
2025-03-19 14:24:03 +0700

    Fix Jetstream Layouts and Add Migration

---
+++ database/migrations/2025_03_19_071145_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('body');
+            $table->string('slug')->unique();
+            $table->string('thumbnail')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('featured')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
